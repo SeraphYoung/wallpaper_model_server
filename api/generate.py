8f8b5c203db3d943e5b3d3cee013698b48dcdc66
杨飞龙 <yangfeilong.beyond@bytedance.com>
2020-07-19 20:17:55 +0800

    new somethings

--- api/generate.py
+++ api/generate.py
@@ -7,6 +7,9 @@ from core import generate
 def image(request):
     j = json.loads(request.data)
     model = j["model"]
+    ct = 0
     for k in app.imageDt:
+        print(ct)
+        ct += 1
         v = app.imageDt.get(k)
         generate.picture(model, k, v)
--- core/fetch.py
+++ core/fetch.py
@@ -4,6 +4,7 @@ from app import directionMatch
 def picture(model, vector):
     key = ','.join([str(a) for a in vector])
     d = directionMatch[key]
+    print(d)
     value = ','.join([str(a) for a in d])
     path = "static/"+model+"/"+value+'.png'
     return path
